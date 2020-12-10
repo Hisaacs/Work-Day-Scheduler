afb712800c43a4024ffd2dd3a10fe0946b9b7a76
Hamish Isaacs <hamisaacs@gmail.com>
2020-12-10 18:27:24 +0800

    Added my initial project

---
+++ README.md
@@ -0,0 +1,25 @@
+Description
+A simple calendar application that allows the user to save an event for each hour in their day.
+
+Introduction
+This application uses the jQuery library to manipulate the HTML elements base on the user's input click events. It also users LocalStorage to store the appointments on click and renders it on the window when the page loads.
+
+Functionality
+The Work Day Scheduler calendar presents a 9AM to 5PM day. The current date is being displayed at the top of the planner. Each of the workday’s hours are colored based on the current time of day. Hours which are in the past are being colored grey, hours in the present is colored red and hours in the future are colored green.
+
+Deployment:
+
+GitHub:
+
+Technologies used
+1.	[Moment.js](https://momentjs.com/) 
+2.	[Bootstrap] (https://getbootstrap.com/) - styling framework used
+3.	HTML (Hypertext Markup Language)
+4.	CSS (Cascading Style Sheets)
+5.	JavaScript (Programming Language)
+
+Video Link
+
+https://www.dropbox.com/s/kegd5k0x5pfdm3i/Work%20Day%20Scheduler%20-%20Google%20Chrome%202020-12-10%2016-17-51.mp4?dl=0
+
+Screenshots
---
+++ script.js
@@ -0,0 +1,86 @@
+// this will display time without any delay when page loads or on referesh
+$("#currentDay").text(moment().format("Do MMMM YYYY, h:mm:ss a"));
+
+// this function displays the current date and time
+setInterval(function () {
+    $("#currentDay").text(moment().format("Do MMMM YYYY, h:mm:ss a"));
+}, 1000)
+
+// variables being declared to create a container | select all of the buttons | Array time and numbers
+const createContainer = $(".container");
+const saveButton = document.querySelectorAll("button");
+const timeList = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+const timeId = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+// a for loop to iterate through the timeList and timeId array and appends the data 
+for (let i = 0; i < timeList.length; i++) {
+    let createRow = $("<div class='row time-block'>").attr("id", timeId[i]);
+    let createTime = $("<div class='hour col-1'>")
+
+
+    // this creates the textarea
+    let createTextarea = $("<textarea class='col-10'>");
+    createTextarea.attr("id", timeList[i]);
+
+    //this creates the buttons
+    let createButton = $("<button type='button' class='saveBtn col-1 far fa-save'>");
+
+    // this appends the created row to the container
+    createContainer.append(createRow);
+    // this create the timeList and appends rows 1-8 starting from (9am and ending at 5pm)
+    createTime.text(timeList[i]);
+    createRow.append(createTime);
+
+    // this create the textarea where user input content
+    // createTextarea.text();
+    createRow.append(createTextarea);
+
+    // this create the buttons from 9am to 5pm
+    createButton.text();
+    createRow.append(createButton);
+}
+
+    // this calls the localStorage Function
+    localStorageFunction();
+
+// this will store the data from the textarea into localStorage
+function localStorageFunction() {
+
+    for (let index = 0; index < numbers.length; index++) {
+        $("textarea")[index].value = localStorage.getItem("textarea" + String(index + 1));
+    }
+}
+
+// this to save the stored data that has been entered into the textarea
+$("button").on("click", function (event) {
+    event.preventDefault();
+
+    for (let index = 0; index < numbers.length; index++) {
+        localStorage.setItem('textarea' + String(index + 1), $("textarea")[index].value)
+    }
+});
+
+// this will color the teaxtarea based on the timeline where the colors 1-3 are defined as the following:-
+// 1. grey equals past the hour
+// 2. red equals current hour
+// 3. green eqauls future hours
+function updateByTheHour() {
+    var currentHour = moment().hours();
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split(" ")[0]);
+
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+        } else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+}
+//this calls the function updateByTheHour
+updateByTheHour();
